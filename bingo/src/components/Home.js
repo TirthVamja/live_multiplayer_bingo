f39f7229780f0773321f803f4c8a255a70e790d0
TirthVamja <vamjatirth@gmail.com>
2024-08-11 15:38:35 +0530

    added dependencies

--- bingo/src/components/Home.js
+++ bingo/src/components/Home.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 import CONFIG from '../config';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme();
 
 export default function SignInSide(props) {
   const [textData, setTextData] = React.useState("");
