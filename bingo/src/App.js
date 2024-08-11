41fac5af425572d7a868963bdb4b0d0eb2bfc0f3
TirthVamja <vamjatirth@gmail.com>
2024-08-11 15:21:29 +0530

    Added Home Page route

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Home from './components/Home';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,12 +11,20 @@ import {
 class App extends Component{
   constructor(props){
     super(props)
+    this.state = {
+      token:"",
+      my_turn:"",
+      navigator:"",
+      click_enable:false
+    }
+    this.setState = this.setState.bind(this);
   }
   render(){
     return (
       <div className='App'>
         <Router>
           <Routes>
+            <Route exact path="/" element={<Home generateTokenState={this.setState}/>} />
           </Routes>
         </Router>
       </div>
---
+++ bingo/src/components/Home.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useNavigate } from "react-router-dom";
+import CONFIG from '../config';
+
+
+export default function SignInSide(props) {
+  const [textData, setTextData] = React.useState("");
+  const navigate = useNavigate();
+  return;
+}
